de6b01bc87a213727d09fe42de5511ede8add0a4
Ben Scholtens <ben.scholtens@me.com>
2023-06-24 01:21:02 -0400

    add file change watcher

--- build.sh
+++ build.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-echo "Running generate_html.py..."
+function generate_html {
+    echo "Running generate_html.py..."
+    python3 -m generate_html
 
-python3 -m generate_html
+    if [ $? -eq 0 ]; then
+        echo "generate_html.py completed successfully."
+    else
+        echo "generate_html.py encountered an error." >&2
+        exit 1
+    fi
+}
 
-if [ $? -eq 0 ]; then
-    echo "generate_html.py completed successfully."
-else
-    echo "generate_html.py encountered an error." >&2
-    exit 1
-fi
+# Initial generation
+generate_html
 
+# Start watcher
 if [[ $1 == "dev" ]]; then
     echo "Starting local server..."
     (
         cd generated
-        python3 -m http.server 8000
+        python3 -m http.server 8000 &
     )
+    echo "Watching markdown files for changes..."
+    watchmedo shell-command \
+        --patterns="*.md" \
+        --recursive \
+        --command='bash -c "./build.sh"' \
+        .
 fi
